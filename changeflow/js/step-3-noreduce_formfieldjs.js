5fe9f5b5294075538f981e6554a59d5cf6f778a7
Alison Howard <alisonhoward05@gmail.com>
2018-03-29 10:53:06 +0100

    Changeflow updated to Jamie's designs
    Step 1 and 2 should be up to date. Step 3 variations need tweaking depending on the journey (change price/room/dates accordingly)

---
+++ changeflow/js/step-3-noreduce_formfieldjs.js
@@ -0,0 +1,34 @@
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+			// run things on page load
+			$(function(){
+				var lnameold = getParameterByName('lnameold');
+				var lnamenew = getParameterByName('lnamenew');
+				var fnameold = getParameterByName('fnameold');
+				var fnamenew = getParameterByName('fnamenew');
+				var cid = getParameterByName('cid');
+				var cim = getParameterByName('cim');
+				var cod = getParameterByName('cod');
+				var com = getParameterByName('com');
+				$(".ci .value").text(cid + "/" + cim + "/2018"); 
+				$(".co .value").text(cod + "/" + com + "/2018"); 
+				
+				if (fnameold != fnamenew) {
+					$(".f-name .value").text(fnamenew);
+					$(".f-name .value").css("background-color", "#fefdca")
+				} else {
+					$(".f-name .value").text(fnameold);
+				}
+				
+
+				if (lnameold != lnamenew) {
+					$(".l-name .value").text(lnamenew);
+					$(".l-name .value").css("background-color", "#fefdca")
+				} else {
+					$(".l-name .value").text(lnameold);
+				}
+			});
