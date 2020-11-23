0e7689e15a83511ac1d1c81a88ebb8bdbf398b81
Craig M <30844627+produx@users.noreply.github.com>
2020-11-23 18:30:48 +0000

    personal

---
+++ personalisation/json-properties-london.js
@@ -0,0 +1,54 @@
+var propertyList = {
+  items: [
+    {
+      name:"Mercure London Paddington",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/10000/8200/8161/c16161a7_z.jpg?impolicy=fcrop&w=320&h=140&q=high",
+      review: [
+        {score:"7.0", str:"Good", count:"422"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          gym: [{hasIt: false, name: "Gym"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          preFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          pool: [{hasIt: true, name: "Indoor Pool"}],
+          parking: [{hasIt: false, name: "Parking available"}],
+          spa: [{hasIt: true, name: "Spa"}],
+          bar: [{hasIt: false, name: "Bar"}],
+          internet: [{hasIt: true, name: "Internet Access"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}],
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"123", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Paddington", lat:"12", lng:""}
+      ]
+    },
+    {
+      name:"Mercure London Paddington 2",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/10000/8200/8161/c16161a7_z.jpg?impolicy=fcrop&w=320&h=140&q=high",
+      review: [
+        {score:"7.0", str:"Good", count:"422"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          gym: [{hasIt: true, name: "Gym"}],
+          restaurant: [{hasIt: false, name: "Restaurant"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"123", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Paddington", lat:"", lng:""}
+      ]
+    }
+  ]
+}
