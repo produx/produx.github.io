49423f601abd4c24d55721fba558f7a7a5715a2b
Craig M <30844627+produx@users.noreply.github.com>
2020-11-27 16:56:07 +0000

    p

--- personalisation/js-functions.js
+++ personalisation/js-functions.js
@@ -5,95 +5,120 @@
 var displayResults = function(data){
 
   $.each(data.items, function(i, item) {
-      var propCard = "";
-      // start card
-      propCard += "<div class='property-wrapper'>"
-      // thumbnail
-      propCard += "<div class='thumb'><img src='" + item.thumb + "' /></div>"
-      // property details
-      propCard += "<div class='property-details'>"
-        propCard += "<div class='name'>" + item.name + "</div>"
-      // location
-        propCard += "<div class='location'><ul>"
-          propCard += "<li class='neighbourhood'>" + item.location[0].neighbourhood + "</li>"
-        propCard += "</ul></div>"
-      // facilities
-      propCard += "<div class='facilities'><ul>";
-      if (requirements.length == 0 && wants.length == 0) {
-        $.each(item.propertyFacilities, function(i, facilities) {
-          $.each(facilities, function(i, facility){
-            if (facility[0].hasIt) {
-              //console.log(facility[0].name)
-              propCard += "<li>" + facility[0].name + "</li>"
-            }
-          });
-        });
-      } else {
-        //propCard += "<li>check against requirements</li>"
-        $.each(item.propertyFacilities, function(i, facilities) {
-          $.each(facilities, function(i, facility){
-            //console.log(facility[0].name)
-            console.log("wants");
-            var n = requirements.includes(facility[0].name);
-            var w = wants.includes(facility[0].name);
-            //console.log(w);
-            if (n){
-              //console.log("needs: " + facility[0].name + " - has it: " + facility[0].hasIt)
-              if (facility[0].hasIt) {
-                propCard += "<li class='yes'>" + facility[0].name + "</li>"
-              } else {
-                propCard += "<li class='no'>" + facility[0].name + "</li>"
-              }
-            };
-            if (w) {
-              console.log("wants: " + facility[0].name + " - has it: " + facility[0].hasIt)
-              if (facility[0].hasIt) {
-                propCard += "<li class='yes'>" + facility[0].name + "</li>"
-              } else {
-                propCard += "<li class='no'>" + facility[0].name + "</li>"
-              }
-            };
-
-            if (!n && !w && facility[0].hasIt){
-              //console.log("else" + facility[0].name);
-              propCard += "<li>" + facility[0].name + "</li>"
-            }
-            //console.log(facility[0].name + ": Wants it: " + n + ", Has it: " + facility[0].hasIt);
-          })
-
+    if (requirements.length > 0) {
+      var meetsNeeds;
+      $.each(item.propertyFacilities, function(i, facilities) {
+        var meetsNeeds = [];
+        $.each(facilities, function(i, facility){
+          var n = requirements.includes(facility[0].name)
+          if (n){
+            meetsNeeds.push(facility[0].hasIt)
+          };
         });
 
-      }
+        if (meetsNeeds.includes(false)){
+          console.log (item.name +": hide")
+        } else {
+          console.log (item.name +": show")
+          buildCard(item);
+        }
 
-      propCard += "</ul></div>"
-      // review and price
-      propCard += "<div class='two-col'>"
-      // reviews
-      propCard += "<div class='reviews'>"
-        propCard += "<div class='review-score'>" + item.review[0].score + "</div>";
-        propCard += "<div class='review-string'>"
-          propCard += "<span class='review-string'>" + item.review[0].str + "</span>";
-          propCard += "<span class='review-count'>" + item.review[0].count + " guest reviews</span>"
-        propCard += "</div>"
-      propCard += "</div>"
-      // price
-      propCard += "<div class='price'>"
-        propCard += "<div class='price-display'>" + item.price[0].currencySymbol + "" + item.price[0].priceDisplay + "</div>";
-        propCard += "<div class='info'>for 2 nights</div>"
-      propCard += "</div>"
-      propCard += "</div></div>"
-      // tools
-      propCard += "<div class='property-tools'>"
-      propCard += "<ul><li><span class='material-icons'>favorite_border</span></li>"
-      propCard += "<li><span class='material-icons'>remove_circle_outline</span></li>"
-      propCard += "<li><a href='' class='more-tools'><span class='material-icons more'>more_horiz</span></a></li>"
-      propCard += "<li><a href=''>Choose Room</a> <span class='material-icons'>arrow_forward</span></li>"
-      propCard += "</ul></div>"
-      // end card
-      propCard += "</div>"
-
-      $("#property-list").append(propCard);
+      });
+    } else {
+      buildCard(item);
+    }
 
   });
 
+}
+
+var buildCard = function(item) {
+  var propCard = "";
+  // start card
+  propCard += "<div class='property-wrapper'>"
+  // thumbnail
+  propCard += "<div class='thumb'><img src='" + item.thumb + "' /></div>"
+  // property details
+  propCard += "<div class='property-details'>"
+    propCard += "<div class='name'>" + item.name + "</div>"
+  // location
+    propCard += "<div class='location'><ul>"
+      propCard += "<li class='neighbourhood'>" + item.location[0].neighbourhood + "</li>"
+    propCard += "</ul></div>"
+  // facilities
+  propCard += "<div class='facilities'><ul>";
+  if (requirements.length == 0 && wants.length == 0) {
+    $.each(item.propertyFacilities, function(i, facilities) {
+      $.each(facilities, function(i, facility){
+        if (facility[0].hasIt) {
+          //console.log(facility[0].name)
+          propCard += "<li>" + facility[0].name + "</li>"
+        }
+      });
+    });
+  } else {
+    //propCard += "<li>check against requirements</li>"
+    $.each(item.propertyFacilities, function(i, facilities) {
+      $.each(facilities, function(i, facility){
+        //console.log(facility[0].name)
+        console.log("wants");
+        var n = requirements.includes(facility[0].name);
+        var w = wants.includes(facility[0].name);
+        //console.log(w);
+        if (n){
+          //console.log("needs: " + facility[0].name + " - has it: " + facility[0].hasIt)
+          if (facility[0].hasIt) {
+            propCard += "<li class='yes'>" + facility[0].name + "</li>"
+          } else {
+            propCard += "<li class='no'>" + facility[0].name + "</li>"
+          }
+        };
+        if (w) {
+          console.log("wants: " + facility[0].name + " - has it: " + facility[0].hasIt)
+          if (facility[0].hasIt) {
+            propCard += "<li class='yes'>" + facility[0].name + "</li>"
+          } else {
+            propCard += "<li class='no'>" + facility[0].name + "</li>"
+          }
+        };
+
+        if (!n && !w && facility[0].hasIt){
+          //console.log("else" + facility[0].name);
+          propCard += "<li>" + facility[0].name + "</li>"
+        }
+        //console.log(facility[0].name + ": Wants it: " + n + ", Has it: " + facility[0].hasIt);
+      })
+
+    });
+
+  }
+
+  propCard += "</ul></div>"
+  // review and price
+  propCard += "<div class='two-col'>"
+  // reviews
+  propCard += "<div class='reviews'>"
+    propCard += "<div class='review-score'>" + item.review[0].score + "</div>";
+    propCard += "<div class='review-string'>"
+      propCard += "<span class='review-string'>" + item.review[0].str + "</span>";
+      propCard += "<span class='review-count'>" + item.review[0].count + " guest reviews</span>"
+    propCard += "</div>"
+  propCard += "</div>"
+  // price
+  propCard += "<div class='price'>"
+    propCard += "<div class='price-display'>" + item.price[0].currencySymbol + "" + item.price[0].priceDisplay + "</div>";
+    propCard += "<div class='info'>for 2 nights</div>"
+  propCard += "</div>"
+  propCard += "</div></div>"
+  // tools
+  propCard += "<div class='property-tools'>"
+  propCard += "<ul><li><span class='material-icons'>favorite_border</span></li>"
+  propCard += "<li><span class='material-icons'>remove_circle_outline</span></li>"
+  propCard += "<li><a href='' class='more-tools'><span class='material-icons more'>more_horiz</span></a></li>"
+  propCard += "<li><a href=''>Choose Room</a> <span class='material-icons'>arrow_forward</span></li>"
+  propCard += "</ul></div>"
+  // end card
+  propCard += "</div>"
+
+  $("#property-list").append(propCard);
 }
--- personalisation/json-properties-london.js
+++ personalisation/json-properties-london.js
@@ -100,7 +100,7 @@ var propertyList = {
         {priceDisplay:"236", currencySymbol:"$"}
       ],
       location: [
-        {neighbourhood: "Canary Wharf", lat:"51.510300", lng:"-0.132290", central:false}
+        {neighbourhood: "Canary Wharf", lat:" E14 9TP", lng:"-0.132290", central:false}
       ],
       theme: [
         {iconic: false, unique: false, theatreDeals: false, homes: false}
@@ -135,7 +135,7 @@ var propertyList = {
         {priceDisplay:"294", currencySymbol:"$"}
       ],
       location: [
-        {neighbourhood: "Westminster", lat:"51.510300", lng:"-0.132290", central:true}
+        {neighbourhood: "Westminster", lat:"SW1W 0PS", lng:"-0.132290", central:true}
       ],
       theme: [
         {iconic: false, unique: false, theatreDeals: false, homes: false}
@@ -170,7 +170,177 @@ var propertyList = {
         {priceDisplay:"113", currencySymbol:"$"}
       ],
       location: [
-        {neighbourhood: "Westminster", lat:"51.510300", lng:"-0.132290", central:true}
+        {neighbourhood: "Westminster", lat:"W8 5SY", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    }, {
+      id: "6",
+      name:"The Tower Hotel",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/10000/2500/2479/43d7b4a0_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"8.4", str:"Very Good", count:"3182"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: false, name: "Pool"}],
+          parking: [{hasIt: true, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: false, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          gym: [{hasIt: false, name: "Gym"}],
+          spa: [{hasIt: false, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: true, name: "Meeting facilities"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"137", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Tower Hill", lat:"E1W 1LD", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    }, {
+      id: "7",
+      name:"Millennium Gloucester Hotel London Kensington",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/530000/523300/523228/3f8aea67_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"7.4", str:"Good", count:"1734"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: false, name: "Pool"}],
+          parking: [{hasIt: true, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: false, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: false, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"120", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "South Kensington", lat:"SW7 4LH", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    }, {
+      id: "8",
+      name:"Park Plaza Westminster Bridge London",
+      thumb: "https://exp.cdn-hotels.com/hotels/4000000/3120000/3113100/3113039/34bd5809_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"8.6", str:"Fabulous", count:"4779"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: false, name: "Pool"}],
+          parking: [{hasIt: false, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: false, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: false, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"217", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Bishop's", lat:"SE1 7UT", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    }, {
+      id: "9",
+      name:"The Montcalm London Marble Arch",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/20000/17700/17700/dea61e39_y.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"8.6", str:"Fabulous", count:"198"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: true, name: "Pool"}],
+          parking: [{hasIt: false, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: false, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: true, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: false, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"217", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "City of Westminster", lat:"W1H 7TN", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    }, {
+      id: "10",
+      name:"One Aldwych",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/120000/116000/115923/1ebfde73_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"9.2", str:"Fabulous", count:"124"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: true, name: "Pool"}],
+          parking: [{hasIt: true, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: false, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: true, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: false, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"608", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Covent Garden", lat:"WC2B 4BZ", lng:"-0.132290", central:true}
       ],
       theme: [
         {iconic: false, unique: false, theatreDeals: false, homes: false}
