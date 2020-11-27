1ff730ed5ef0a7871b70687079f019260d30e275
Craig M <30844627+produx@users.noreply.github.com>
2020-11-27 15:49:22 +0000

    p

--- personalisation/js-functions.js
+++ personalisation/js-functions.js
@@ -18,15 +18,53 @@ var displayResults = function(data){
           propCard += "<li class='neighbourhood'>" + item.location[0].neighbourhood + "</li>"
         propCard += "</ul></div>"
       // facilities
-      propCard += "<div class='facilities'><ul>"
-      $.each(item.propertyFacilities, function(i, facilities) {
-        $.each(facilities, function(i, facility){
-          if (facility[0].hasIt) {
-            console.log(facility[0].name)
-            propCard += "<li>" + facility[0].name + "</li>"
-          }
+      propCard += "<div class='facilities'><ul>";
+      if (requirements.length == 0 && wants.length == 0) {
+        $.each(item.propertyFacilities, function(i, facilities) {
+          $.each(facilities, function(i, facility){
+            if (facility[0].hasIt) {
+              //console.log(facility[0].name)
+              propCard += "<li>" + facility[0].name + "</li>"
+            }
+          });
         });
-      });
+      } else {
+        //propCard += "<li>check against requirements</li>"
+        $.each(item.propertyFacilities, function(i, facilities) {
+          $.each(facilities, function(i, facility){
+            //console.log(facility[0].name)
+            console.log("wants");
+            var n = requirements.includes(facility[0].name);
+            var w = wants.includes(facility[0].name);
+            //console.log(w);
+            if (n){
+              //console.log("needs: " + facility[0].name + " - has it: " + facility[0].hasIt)
+              if (facility[0].hasIt) {
+                propCard += "<li class='yes'>" + facility[0].name + "</li>"
+              } else {
+                propCard += "<li class='no'>" + facility[0].name + "</li>"
+              }
+            };
+            if (w) {
+              console.log("wants: " + facility[0].name + " - has it: " + facility[0].hasIt)
+              if (facility[0].hasIt) {
+                propCard += "<li class='yes'>" + facility[0].name + "</li>"
+              } else {
+                propCard += "<li class='no'>" + facility[0].name + "</li>"
+              }
+            };
+
+            if (!n && !w && facility[0].hasIt){
+              //console.log("else" + facility[0].name);
+              propCard += "<li>" + facility[0].name + "</li>"
+            }
+            //console.log(facility[0].name + ": Wants it: " + n + ", Has it: " + facility[0].hasIt);
+          })
+
+        });
+
+      }
+
       propCard += "</ul></div>"
       // review and price
       propCard += "<div class='two-col'>"
--- personalisation/json-properties-london.js
+++ personalisation/json-properties-london.js
@@ -10,16 +10,18 @@ var propertyList = {
       propertyType:"",
       propertyFacilities: [
         {
-          gym: [{hasIt: false, name: "Gym"}],
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: true, name: "Pool"}],
+          parking: [{hasIt: false, name: "Parking"}],
+          breakfast: [{hasIt: true, name: "Breakfast included"}],
+          freeCancel: [{hasIt: true, name: "Free cancellation"}],
           restaurant: [{hasIt: true, name: "Restaurant"}],
           petFriendly: [{hasIt: false, name: "Pet Friendly"}],
-          pool: [{hasIt: true, name: "Indoor Pool"}],
-          parking: [{hasIt: false, name: "Parking available"}],
+          gym: [{hasIt: false, name: "Gym"}],
           spa: [{hasIt: true, name: "Spa"}],
           bar: [{hasIt: false, name: "Bar"}],
-          internet: [{hasIt: true, name: "Internet Access"}],
           meeting: [{hasIt: false, name: "Meeting facilities"}],
-          bathtub: [{hasIt: true, name: "Bathtub"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}]
         }
       ],
       roomFacilities: [],
@@ -27,7 +29,7 @@ var propertyList = {
         {priceDisplay:"123", currencySymbol:"$"}
       ],
       location: [
-        {neighbourhood: "Paddington", lat:"51.517101", lng:"-0.172910"}
+        {neighbourhood: "Paddington", lat:"51.517101", lng:"-0.172910", central:false}
       ],
       theme: [
         {iconic: false, unique: false, theatreDeals: false, homes: false}
@@ -43,16 +45,18 @@ var propertyList = {
       propertyType:"",
       propertyFacilities: [
         {
-          gym: [{hasIt: false, name: "Gym"}],
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: false, name: "Pool"}],
+          parking: [{hasIt: false, name: "Parking"}],
+          breakfast: [{hasIt: true, name: "Breakfast included"}],
+          freeCancel: [{hasIt: false, name: "Free cancellation"}],
           restaurant: [{hasIt: true, name: "Restaurant"}],
           petFriendly: [{hasIt: false, name: "Pet Friendly"}],
-          pool: [{hasIt: false, name: "Indoor Pool"}],
-          parking: [{hasIt: false, name: "Parking available"}],
+          gym: [{hasIt: true, name: "Gym"}],
           spa: [{hasIt: false, name: "Spa"}],
           bar: [{hasIt: true, name: "Bar"}],
-          internet: [{hasIt: false, name: "Internet Access"}],
           meeting: [{hasIt: false, name: "Meeting facilities"}],
-          bathtub: [{hasIt: false, name: "Bathtub"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}]
         }
       ],
       roomFacilities: [],
@@ -60,7 +64,113 @@ var propertyList = {
         {priceDisplay:"123", currencySymbol:"$"}
       ],
       location: [
-        {neighbourhood: "Piccadilly", lat:"51.510300", lng:"-0.132290"}
+        {neighbourhood: "Piccadilly", lat:"51.510300", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    },
+    //background-image:url('https://exp.cdn-hotels.com/hotels/17000000/16190000/16181400/16181347/ee8539eb_z.jpg?impolicy=fcrop&w=250&h=140&q=high')
+    {
+      id: "3",
+      name:"Novotel London Canary Wharf Hotel",
+      thumb: "https://exp.cdn-hotels.com/hotels/17000000/16190000/16181400/16181347/ee8539eb_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"9.2", str:"Superb", count:"834"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: true, name: "Pool"}],
+          parking: [{hasIt: false, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: true, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: true, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: false, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: false, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"236", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Canary Wharf", lat:"51.510300", lng:"-0.132290", central:false}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    },
+    {
+      id: "4",
+      name:"The Rubens At The Palace",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/530000/528400/528337/16156f8c_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"9.2", str:"Superb", count:"897"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: false, name: "Pool"}],
+          parking: [{hasIt: true, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: true, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: true, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: false, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: false, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"294", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Westminster", lat:"51.510300", lng:"-0.132290", central:true}
+      ],
+      theme: [
+        {iconic: false, unique: false, theatreDeals: false, homes: false}
+      ]
+    },
+    {
+      id: "5",
+      name:"Copthorne Tara Hotel London Kensington",
+      thumb: "https://exp.cdn-hotels.com/hotels/1000000/30000/26200/26145/fd197ef7_z.jpg?impolicy=fcrop&w=420&h=120&q=high",
+      review: [
+        {score:"7.8", str:"Good", count:"1598"}
+      ],
+      propertyType:"",
+      propertyFacilities: [
+        {
+          wifi: [{hasIt: true, name: "Wifi"}],
+          pool: [{hasIt: false, name: "Pool"}],
+          parking: [{hasIt: true, name: "Parking"}],
+          breakfast: [{hasIt: false, name: "Breakfast included"}],
+          freeCancel: [{hasIt: true, name: "Free cancellation"}],
+          restaurant: [{hasIt: true, name: "Restaurant"}],
+          petFriendly: [{hasIt: false, name: "Pet Friendly"}],
+          gym: [{hasIt: true, name: "Gym"}],
+          spa: [{hasIt: false, name: "Spa"}],
+          bar: [{hasIt: true, name: "Bar"}],
+          meeting: [{hasIt: false, name: "Meeting facilities"}],
+          bathtub: [{hasIt: true, name: "Bathtub"}]
+        }
+      ],
+      roomFacilities: [],
+      price: [
+        {priceDisplay:"113", currencySymbol:"$"}
+      ],
+      location: [
+        {neighbourhood: "Westminster", lat:"51.510300", lng:"-0.132290", central:true}
       ],
       theme: [
         {iconic: false, unique: false, theatreDeals: false, homes: false}
